384a0d983099578917d0974110200a0738249bb4
Nerbys <git@nerbys.email>
2025-12-10 11:47:48 +0100

    fix: Completely disable bulk create module
    - Removed bulk create from security CSV
    - Commented out bulk create import
    - Module now works standalone without supplier dependencies

--- webshop_product_creator/wizard/__init__.py
+++ webshop_product_creator/wizard/__init__.py
@@ -1,7 +1,3 @@
 from . import product_quick_create
-
-# Only import bulk create if supplier module is available
-try:
-    from . import product_bulk_create
-except:
-    pass
+# Bulk create disabled - requires supplier_pricelist_sync module
+# from . import product_bulk_create
